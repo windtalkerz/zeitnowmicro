c6ae3314c4b1010173a91eaa6fbbf4587e1c73cc
windtalkerz <windtalkerz@me.com>
2018-12-04 09:11:46 +0100

    update master

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,15 @@
 #! /bin/bash
 
-cd ~/Desktop/zeitnowmicro
-now && now alias
+# cd ~/Desktop/zeitnowmicro
+# now && now alias
 
+
+
+
+# ###################
+# -> GITHUB
+# ###################
+git add .
+git commit -m "update master"
+git push origin master
+echo "successfully updated project check deployment status on https://micro.supermario.io"
